ec1e050f57db1b4a7345d9463db57099974ac234
maduranga20 <madurangadesign22@gmail.com>
2024-09-19 15:58:08 +0530

    Async changes

--- src/Component/GetData.js
+++ src/Component/GetData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
  class AsyncData extends Component {
+ 
     constructor(props) {
         super(props);
         this.state = {
@@ -13,21 +14,22 @@ import React, { Component } from 'react'
             const BASE_URL = "https://fakestoreapi.com/products?limit=5";
             const response = await fetch(BASE_URL);
             const data = await response.json();
+            console.log(data);
+            
             this.setState({posts:data})
             
           }
-       
-      
+        
           
   render() {
-    console.log(this.state.posts[0]);
+   
     return (
         <div>
                 
         {this.state.posts.map((post) => (
            <div className="post" key={post.id}>
           <h3>{post.title}</h3>
-           <p></p>
+           <p>{post.price}</p>
            <img src={post.image} alt='product'/>
           </div>
            ))}
