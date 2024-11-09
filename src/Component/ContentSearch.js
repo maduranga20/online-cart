b88579251d241edd76c446799fe83c60ead46b6e
maduranga20 <madurangadesign22@gmail.com>
2024-11-09 09:25:39 +0530

    add new files and  git ignore changes

---
+++ src/Component/ContentSearch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import AsyncData from './GetData'
+
+
+export default class ContentSearch extends Component {
+  render() {
+    // const apiKey = process.env.APIKey;
+    
+  
+
+    return (
+      <div></div>
+    )
+  }
+}
--- src/Component/GetData.js
+++ src/Component/GetData.js
@@ -1,45 +1,53 @@
 import React, { Component } from 'react'
 
- class AsyncData extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          get: [],
-          insert:"",
-          content:""
-         };
-         this.InsertData = this.InsertData.bind(this);
-        }  
-        async componentDidMount() {
-            const BASE_URL = "https://fakestoreapi.com/products?limit=5";
-            const response = await fetch(BASE_URL);
-            const data = await response.json();
-            this.setState({get:data})
-            
-          }
-       
-          InsertData=(event)=>{
-            this.setState({insertValue:event.target.value})
-          }
-          
-  render() {
-    
+class AsyncData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      get: [],
+      insert: "",
+      content: ""
+    };
+    this.InsertData = this.InsertData.bind(this);
+  }
+  async componentDidMount() {
+    const BASE_URL = "https://fakestoreapi.com/products?limit=10";
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+    this.setState({ get: data })
 
-    
+  }
+
+  InsertData = (event) => {
+    this.setState({ insert: event.target.value })
+  }
+  SubmitData = () => {
+    this.setState({ content: this.state.insert });
+  }
+
+
+  render() {
+    let parseContent = parseInt(this.state.content, 10);
+    const selectPrice = this.state.get.filter(item => item.price > parseContent);
+    // console.log(selectPrice);
+    console.log(this.state.get);
+
+
     return (
-     
-        <div>
-        <input type='text' onChange={InsertData}/>       
-         
-       
-        {this.state.get.map((item) => (
-           <div className="post" key={item.id}>
-          <h3>{item.title}</h3>
-           <p></p>
-           <img src={item.image} alt='product'/>
+
+      <div>
+        <input type='text' onChange={this.InsertData} />
+        <button onClick={this.SubmitData}>Search</button>
+
+
+        {selectPrice.map((item) => (
+          <div className="post" key={item.id}>
+            <h3>{item.title}</h3>
+
+            <img src={item.image} alt='product' />
           </div>
-           ))}
-          
+        ))}
+
       </div>
 
     )
